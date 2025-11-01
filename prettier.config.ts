c998c85c992c2e27ee67931246d144ca5d99ee8f
liili <liili_cn@hotmail.com>
2025-11-01 22:33:05 +0800

    :play-circle: scripts: 使用 Prettier

---
+++ prettier.config.ts
@@ -0,0 +1,27 @@
+import { type Config } from "prettier";
+
+const config: Config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "as-needed",
+  jsxSingleQuote: false,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  objectWrap: "preserve",
+  bracketSameLine: false,
+  arrowParens: "always",
+  requirePragma: false,
+  insertPragma: false,
+  checkIgnorePragma: false,
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  vueIndentScriptAndStyle: false,
+  endOfLine: "lf",
+  embeddedLanguageFormatting: "auto",
+  singleAttributePerLine: false,
+};
+
+export default config;
